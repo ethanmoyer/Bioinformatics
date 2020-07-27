fc24ce4cf66f37b47c270da8101959799eb8dbe8
ethan <57536747+ethanmoyer@users.noreply.github.com>
2020-07-27 11:29:36 -0400

    working on merging edges

--- constructSyntenyBlocks.py
+++ constructSyntenyBlocks.py
@@ -21,6 +21,7 @@ def create_mker_grid(seq0, seq1, k):
 
 	grid = np.array([[1 if kmer1 == kmer0 or kmer0 == kmer_list1_rc[::-1][i][::-1] else 0 for i, kmer1 in enumerate(kmer_list1[::-1])] for kmer0 in kmer_list0])
 
+	return grid
 
 def distance(p_i, p_j):
 	return np.sqrt((p_i[0] - p_j[0])**2 + (p_i[1] - p_j[1])**2)
@@ -34,7 +35,7 @@ def get_synteny_blocks(grid, max_distance=2, min_size=3):
 
 	n = len(x_coords)
 
-	synteny_blocks = []
+	synteny_blocks_coords = []
 	for c, coords_i in enumerate(zip(x_coords[:n - 1], y_coords[:n - 1])):
 		for coords_j in zip(x_coords[c + 1:], y_coords[c + 1:]):
 			x_i = coords_i[0]
@@ -44,7 +45,22 @@ def get_synteny_blocks(grid, max_distance=2, min_size=3):
 			y_j = coords_j[1]
 
 			if distance((x_i, y_i), (x_j, y_j)) <= max_distance:
-				synteny_blockss.append([coords_i, coords_j])
+				synteny_blocks_coords.append([coords_i, coords_j])
+
+	# Merge the blocks
+	synteny_blocks = []
+	n = len(synteny_blocks_coords)
+	for b, synteny_block_i in enumerate(synteny_blocks_coords[:n - 1]):
+		synteny_block_max = max(synteny_block_i)
+		for synteny_block_j in synteny_blocks_coords[b + 1:]:
+			print(synteny_block_max)
+			print(synteny_block_j)
+			if synteny_block_max == min(synteny_block_j):
+				synteny_block_max = max(synteny_block_j)
+
+		synteny_blocks.append([min(synteny_block_i), synteny_block_max])
+		print(synteny_blocks)
+		quit()
 
 	return [synteny_block for synteny_block in synteny_blocks if distance(min(synteny_block), max(synteny_block)) >= min_size]
 
@@ -56,3 +72,5 @@ if __name__ == '__main__':
 	grid = create_mker_grid(seq0, seq1, 3)
 
 	synteny_blockss = get_synteny_blocks(grid)
+
+
